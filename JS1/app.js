8d5ddc720987fc96d732dda260edc9f75fed0804
10-Anas <anasq0q@gmail.com>
2022-03-20 06:45:12 -0700

    scriblles

---
+++ JS1/app.js
@@ -0,0 +1,11 @@
+let x=prompt("please enter your age");
+let y=Number (x);
+console.log(x+1);
+console.log(typeof x);
+console.log(y+3);
+if (x !=null){
+    console.log("is " + x + " Your age?");
+}
+else{
+    console.log("goodBye");
+}
