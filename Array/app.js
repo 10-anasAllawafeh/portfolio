c9da07bb2a2534c7e76dd4863dbbe605973119d6
10-Anas <anasq0q@gmail.com>
2022-03-21 12:30:42 -0700

    array exercises

---
+++ Array/app.js
@@ -0,0 +1,52 @@
+`use strict`;
+// let selected_color=["red","blue","green","brown"];
+// selected_color.push("yellow");
+// console.log(selected_color);
+// document.write(selected_color);
+// console.log(selected_color[0]);
+// console.log(selected_color[2]);
+// selected_color[1]="orange";
+// console.log(selected_color);
+// console.log(selected_color[1]);
+// let type=typeof selected_color;
+// console.log(type);
+// console.log(Array.isArray(selected_color));
+// console.log(selected_color.length);
+
+// let a=1;
+// while(a<=5)
+// {
+// console.log("Hello");
+// a++;  
+
+// }
+
+// let a=10;
+// do{
+//     console.log(a);
+//     a++;
+    
+// } while(a<=10);
+    
+// let sum=0;
+// for (let a=0; a<3; a++)
+// {
+//     sum=sum + a;   
+// }
+// document.write(sum);
+// console.log(sum);
+
+// function area(width,height) {
+//     let
+    
+// }
+
+
+// function fullName (){
+//     let Name1=prompt("Please enter your first Name");
+//     let Name2=prompt("Please enter your Last Name");
+    
+//     console.log("welcom "+ Name1 + Name2);
+//     }    
+
+
