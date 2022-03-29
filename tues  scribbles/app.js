956f701be64c222e55b675042eb7ba9a25ed07d5
10-Anas <anasq0q@gmail.com>
2022-03-29 09:03:33 -0700

    api example

---
+++ tues  scribbles/app.js
@@ -0,0 +1,38 @@
+`use strict`;
+function hline() {
+    document.write(`<br> <hr>`);
+}
+// let para=document.createElement(`li`);
+// ul1.appendChild(para);
+// para.id=`p1`;
+
+
+async function getData() {
+    
+    let responce= await fetch(`https://eu1.locationiq.com/v1/search.php?key=pk.5fdf7554df7d097ac56a7287ed04a614&q=aqaba&format=json`);
+    // console.log(responce);
+    let data= await responce.json();
+    console.log(data);
+
+    let output=``;
+    
+    displayName=data.map((name_1,index) => 
+    {
+    let new_name=name_1.display_name.split(",",2);
+    // let new_name2=name_1.display_name.slice(0,26);
+    output += `<li>display_name: ${new_name}  ||  Longitude: ${name_1.lon.slice(0,2)}  ||  Latitude: ${name_1.lat.slice(0,2)}<br><hr></li>`}
+
+);
+    console.log(displayName);
+    document.body.innerHTML=output;
+
+
+
+
+    // displaylon=data.map((lon_1,index) => output += `<li>Longitude: ${lon_1.lon}</li>`);
+    // document.body.innerHTML=output;
+    // displaylat=data.map((lat_1,index) => output += `<li>Latitude: ${lat_1.lat}</li>`);
+    // document.body.innerHTML=output;
+}
+getData();
+
